9f12720ba79d16dea215133825bd1efecbe4ff1c
Grinik767 <grigorikiseleff@yandex.ru>
2022-04-22 18:36:26 +0500

    Тестовая v2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 import os
 import shutil
 from func import get_images, get_vectors_from_images, tokenize_text, get_vector_from_tokens, get_result
@@ -11,9 +12,16 @@ chats = {}
 @bot.message_handler(commands=["start"])
 def start(message):
     if message.chat.id not in chats:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("/clear")
+        btn2 = types.KeyboardButton("/end")
+        markup.add(btn1, btn2)
         chats[message.chat.id] = {'proc': False, 'txt': '', 'pht': 0}
-        bot.send_message(message.chat.id,
-                         'Здравствуйте!\nОтправьте мне данные для анализа.\nПосле окончания ввода введите /end')
+        bot.send_message(message.chat.id, 'Здравствуйте!'
+                                          '\nОтправьте мне данные для анализа.'
+                                          '\nДля сброса фото введите\n/clear'
+                                          '\nПосле окончания ввода введите\n/end', reply_markup=markup)
+
     else:
         bot.send_message(message.chat.id, "Команда сейчас не доступна")
 
@@ -22,18 +30,39 @@ def start(message):
 def end(message):
     if message.chat.id in chats and not chats[message.chat.id]['proc'] and (
             chats[message.chat.id]['txt'] and chats[message.chat.id]['pht']):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("/start")
+        markup.add(btn1)
         bot.send_message(message.chat.id, "Данные приняты для анализа")
         chats[message.chat.id]['proc'] = True
         images = get_vectors_from_images(get_images(message.chat.id))
         texts = get_vector_from_tokens(*tokenize_text(chats[message.chat.id]['txt']))
         result = get_result(images, texts)[0] * 100
-        bot.send_message(message.chat.id, f"Результат анализа - {result[0]:.3f}%")
+        bot.send_message(message.chat.id, f"Результат анализа - {result[0]:.3f}%", reply_markup=markup)
         del chats[message.chat.id]
         shutil.rmtree(f'data/{message.chat.id}')
     elif not (message.chat.id in chats):
-        bot.send_message(message.chat.id, "Чтобы бот начал работу, введите /start")
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("/start")
+        markup.add(btn1)
+        bot.send_message(message.chat.id, "Чтобы бот начал работу, введите /start", reply_markup=markup)
     elif chats[message.chat.id]['proc']:
         bot.send_message(message.chat.id, "Бот занят! Скоро будет предсказание)")
+    else:
+        bot.send_message(message.chat.id, "Команда сейчас не доступна")
+
+
+@bot.message_handler(commands=["clear"])
+def clear(message):
+    if message.chat.id in chats and not chats[message.chat.id]['proc'] and chats[message.chat.id]['pht']:
+        chats[message.chat.id]['pht'] = 0
+        shutil.rmtree(f'data/{message.chat.id}')
+        bot.send_message(message.chat.id, "Фото удалены")
+    elif not (message.chat.id in chats):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("/start")
+        markup.add(btn1)
+        bot.send_message(message.chat.id, "Чтобы бот начал работу, введите /start", reply_markup=markup)
     else:
         bot.send_message(message.chat.id, "Команда сейчас не доступна")
 
@@ -48,7 +77,10 @@ def handle_text(message):
             bot.send_message(message.chat.id, "Текст изменён")
         chats[message.chat.id]['txt'] = new_text
     elif not (message.chat.id in chats):
-        bot.send_message(message.chat.id, "Чтобы бот начал работу, введите /start")
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("/start")
+        markup.add(btn1)
+        bot.send_message(message.chat.id, "Чтобы бот начал работу, введите /start", reply_markup=markup)
     elif chats[message.chat.id]['proc']:
         bot.send_message(message.chat.id, "Бот занят! Скоро будет предсказание)")
 
@@ -68,7 +100,10 @@ def handle_docs_document(message):
             new_file.write(downloaded_file)
         bot.send_message(message.chat.id, "Фото принято")
     elif not (message.chat.id in chats):
-        bot.send_message(message.chat.id, "Чтобы бот начал работу, введите /start")
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("/start")
+        markup.add(btn1)
+        bot.send_message(message.chat.id, "Чтобы бот начал работу, введите /start", reply_markup=markup)
     elif chats[message.chat.id]['proc']:
         bot.send_message(message.chat.id, "Бот занят! Скоро будет предсказание)")
     elif chats[message.chat.id]['pht'] == 10:
